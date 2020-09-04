c07a355ddddaf4ba1f6f1d9d365020a678b8aab8
Stoyan Spasov <spasovstoyan@gmail.com>
2020-09-04 15:43:34 +0300

    connecting website with contenful

---
+++ src/Contentful.js
@@ -0,0 +1,7 @@
+import { createClient } from "contentful";
+
+export default createClient({
+space: "cxv3xusgjvmq",
+accessToken: "GOavsbTr8QGsO5VG4MRMbwL73NJbE1G_wo5blmBxjc0"
+})
+
--- src/context.js
+++ src/context.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import items from "./data";
+// import items from "./data";
+import Client from "./Contentful";
+
 
 const RoomContext = React.createContext();
 
@@ -23,22 +25,35 @@ class RoomProvider extends Component {
     };
 
     //getData
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "abuDhabiBeachResort",
+                order: "sys.createdAt"
+            });
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     componentDidMount() {
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
-
-        this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        })
+        this.getData()
     }
 
     formatData(items) {
